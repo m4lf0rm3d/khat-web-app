f72c042379d6d15c25016dd187491ee0d815ff21
Arsal <mh07607@st.habib.edu.pk>
2024-04-02 15:34:53 +0500

    LongButton class

--- Frontend/src/components/LongButton.jsx
+++ Frontend/src/components/LongButton.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import "./LongButton.css"
 
-const LongButton = () => {
-
+const LongButton = ({text, active}) => {
+    return (
+        <button className="btn" style={{backgroundColor: active? "white" : "#E1C9B0"}}>
+            {text}
+        </button>
+    );
 }
 
 export default LongButton;
--- Frontend/src/pages/Signup/Index.jsx
+++ Frontend/src/pages/Signup/Index.jsx
@@ -1,34 +1,48 @@
-import React from "react";
-import "./Signup.css"
+    import React from "react";
+    import "./Signup.css"
+    import LongButton from "../../components/LongButton";
 
-export const Signup= () =>  {
-    const phoneCodes = [
-        '+92',
-        '+91',
-        '+90'
-    ]
+    export const Signup= () =>  {
+        const phoneCodes = [
+            '+92',
+            '+91',
+            '+90'
+        ]
 
-    return (
-        <>
-        <div className="main">
-            <div className="header">Create Account</div>
-            <div className="codePrompt"> Enter your email and phone number, and we'll send you a code to create your account.</div>
-            <div className="getPhoneNumber">
-                <div className="inputHeader">Phone Number</div>
-                <select name="phone-codes" id="phone-codes">
-                    {phoneCodes.map(
-                        (phoneCode, index) => <option key={index} value={phoneCode}>{phoneCode}</option> 
-                    )}
-                </select>
-                <input className="simpleInput" type="text" />
+        return (
+            <>
+            <div className="main">
+                <div className="upper">
+                    <div className="header">Create Account</div>
+                    <div className="codePrompt"> Enter your email and phone number, and we'll send you a code to create your account.</div>
+                    <div className="getPhoneNumber">
+                        <div className="inputHeader">Phone Number</div>
+                        <div className="inputSection">
+                            <select className="dropDown" name="phone-codes" id="phone-codes">
+                                {phoneCodes.map(
+                                    (phoneCode, index) => <option key={index} value={phoneCode}>{phoneCode}</option> 
+                                )}
+                            </select>
+                            <input className="simpleInput" type="text" />
+                        </div>
+                    </div>
+                    <div className="getPhoneNumber">
+                        <div className="inputHeader">Email</div>
+                        <input className="simpleInput" type="text" placeholder="a@b.com" />
+                    </div>
+                    
+                    <div className="verifyBtn">
+                        <LongButton text="Verify" active={false}/>
+                    </div>
+                </div>
+                <div className="footer">
+                    <div className="footerText">
+                        Already have an account?
+                    </div>
+                    <a href="">Log in</a>
+                </div>     
             </div>
-            <div className="inputHeader">Email</div>
-            <input className="simpleInput" type="text" />
-            <div className="verifyBtn">
-                <button>Verify</button>
-            </div>            
-        </div>
-        </>
-    );
+            </>
+        );
 
-}
+    }

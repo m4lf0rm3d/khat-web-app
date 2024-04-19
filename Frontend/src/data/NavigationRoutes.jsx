ab2fd260b13d8ea9fc637a73e2f1577ce80ed6b2
Muzz309 <98407381+Muzz309@users.noreply.github.com>
2024-04-19 23:04:41 +0500

    Started Work on Add Companion Page

--- Frontend/src/data/NavigationRoutes.jsx
+++ Frontend/src/data/NavigationRoutes.jsx
@@ -1,5 +1,6 @@
 import Login from "../pages/login/Index";
 import Signup from "../pages/create-account/Index";
+import AddCompanion from "../pages/add-companion/Index";
 import Dashboard from "../pages/dashboard/Index";
 import { DeviceNotSupported } from "../pages/device-not-supported/Index";
 
@@ -13,6 +14,11 @@ export const NAVIGATION_ROUTES = {
         path: "/create-account",
         component: Signup,
         title: "Khat | Create Account",
+    },
+    ADD_COMPANION: {
+        path: "/add-companion",
+        component: AddCompanion,
+        title: "Khat | Add Companion",
     },
     DEVICE_NOT_SUPPORTED: {
         path: "/device-not-supported",
---
+++ Frontend/src/pages/add-companion/Index.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import { withDeviceWidthCheck } from "../../utils/WithDeviceWidthCheck.jsx";
+import { Link, useNavigate } from "react-router-dom";
+import { NAVIGATION_ROUTES } from "../../data/NavigationRoutes.jsx";
+import "../create-account/CreateAccount.css";
+import { Loader } from "../../components/Loader.jsx";
+import { MessagePopup } from "../../components/MessagePopup.jsx";
+import { Helmet } from "react-helmet";
+import { fetchAppSettings } from "../../utils/FetchAppSettings.js";
+
+const AddCompanion = () =>{
+
+    return (
+        <section className="createAccount">
+            <Helmet>
+                <title>{NAVIGATION_ROUTES.ADD_COMPANION.title}</title>
+            </Helmet>
+
+            <div className="addCompanionMain">
+            <h1>Add A Companion!</h1>
+            </div>
+        </section>
+
+    )
+}

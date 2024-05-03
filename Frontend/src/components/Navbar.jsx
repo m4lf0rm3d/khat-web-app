f00336b8005deaa704eb9643dfdc0f7939b36126
Arsal <mh07607@st.habib.edu.pk>
2024-05-03 17:03:22 +0500

    navbar css

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -18,21 +18,21 @@ export const NavBar = () => {
       <ul className="nav-list"> {/* Use a class for styling */}
         <li className={`${location.pathname === "/" ? "active" : ""}`}> {/* Active class for current page */}
           <Link style={{textDecoration: "none"}} to="/">
-            <HomeOutline className="nav-icon" /> Home
+            <HomeOutline className="nav-icon" />
           </Link>
         </li>
         <li className={`${location.pathname === "/add-companion" ? "active" : ""}`}>
           <Link style={{textDecoration: "none"}} to="/add-companion">
-            <PersonAddOutline className="nav-icon" /> Add Companion
+            <PersonAddOutline className="nav-icon" />
           </Link>
         </li>
         <li className={`${location.pathname === "/companions" ? "active" : ""}`}>
           <Link style={{textDecoration: "none"}} to="/companions">
-            <MailOutline className="nav-icon" /> Companions
+            <MailOutline className="nav-icon" /> 
           </Link>
         </li>
         <li style={{textDecoration: "none"}} onClick={handleLogout}>
-          <LogOutOutline className="nav-icon" /> Logout
+          <LogOutOutline className="nav-icon" /> 
         </li>
       </ul>
     </nav>
